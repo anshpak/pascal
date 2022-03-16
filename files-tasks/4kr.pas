dbedf227059ecc263ffab0bf3aa3d02aba81b459
Andrey Shpak <anshpak.by@gmail.com>
2022-03-17 01:05:22 +0300

    add new solution to task: find-k-symbols

---
+++ files-tasks/4kr.pas
@@ -0,0 +1,44 @@
+program findKSymbols;
+const
+        k=15;
+        n=10;
+        L=200;
+        alph='abcdefghijklmnopqrstvwxyz';
+var
+        t: text;
+        f: file of char;
+        i:integer;
+        a: char;
+begin
+        assign(t,'src/symbols.txt');
+        assign(f,'src/symbols.dat');
+        rewrite(t);
+        rewrite(f);
+        {Записываю алфавит в текстовый файл}
+        for i:=1 to L do
+        begin
+                if i mod 26 = 0 then
+                        continue;
+                writeln(t,alph[i mod 26]);
+        end;
+        close(t);
+        {Записываю данные из текстового в типизированный}
+        reset(t);
+        while not eof(t) do
+        begin
+                readln(t,a);
+                write(f,a);
+        end;
+        close(t);
+        {Слово длиной k символов, начиная с n-й позиции}
+        seek(f,n-1);
+        while not eof(f) do
+        begin
+                read(f,a);
+                if filepos(f) = n+k+1 then
+                        break;
+                write(a);
+        end;
+        close(f);
+        readln;
+end.
