26cddad2a9b560c98ac09abc6690988a1ffd755a
Andrey Shpak <anshpak.by@gmail.com>
2022-03-22 01:38:16 +0300

    refactor: change 7.1 lab name to just 7 lab

--- labs/7.pas
+++ labs/7.pas
@@ -1,30 +1,11 @@
-program seventhsLab;
-uses calc7lab;
-{const
-        n=78;
-        x=0.5;}
-var
-        n: integer;
-        x: real;
-
-procedure readValFromFile(var n: integer; var x: real; path: string);
-var
-        t: text;
-        temp1,temp2: string;
-
+program seventhLab;
+uses calc7Lab;
+const
+        n=25;
+        x=0.2;
 begin
-        assign(t,path);
-        while not eof(t) do
-                read(t,temp1,temp2);
-        val(temp1,n);
-        val(temp2,x);
-        close(t);
-end;
-
-begin
-        {writeln(getCalcResAndPrint(n,x):60);}
-        readValFromFile(n,x,'lab-7-data.txt');
-        writeln(n);
-        writeln(x);
-        readln();
+        printHead('src/calcRes.txt');
+        calcWriteExpr(n, x, 'src/calcRes.txt');
+        write('Подсчет выполнен. Пожалуйста, проверьте файл calcRes.txt');
+        readln;
 end.
