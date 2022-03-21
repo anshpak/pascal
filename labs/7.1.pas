38b90ee303a52e93f7bd8a953c88b0221ecf2b07
Andrey Shpak <anshpak.by@gmail.com>
2022-03-21 23:56:33 +0300

    feat: finally understand how approximate calculation of thw sum works

--- labs/7.1.pas
+++ labs/7.1.pas
@@ -1,10 +1,10 @@
 program seventhLab;
 const
-        n=15;
+        n=200;
         size=9;
         degrees : array [1..size] of integer = (3, 9, 27, 81, 243, 729, 2187, 6561, 19683);
 
-function getNumLen(num: byte): byte;
+function getNumLen(num: integer): byte;
 var
         strTmp: string;
 begin
@@ -12,14 +12,23 @@ begin
         getNumLen:=Length(strTmp);
 end;
 
-procedure calcExpr(n: byte);
+function calcExpr(n: integer): real;
 var
-
+        a, S: real;
+        i: integer;
 begin
-
+        S:=0;
+        a:=1;
+        for i:=1 to n do
+        begin
+                S:=S+a;
+                writeln(S);
+                a:=(a*i)/(i+1);
+        end;
+        calcExpr:=S;
 end;
 
 begin
-
+        writeln(calcExpr(n));
         readln;
 end.
