e8e77eb9dfbf2026ac89b2c2079e2988419fa59b
Andrey Shpak <anshpak.by@gmail.com>
2022-03-29 12:16:30 +0300

    feat: update lab

--- labs/7.pas
+++ labs/7.pas
@@ -1,20 +1,28 @@
 program seventhLab;
 uses calc7Lab;
 const
-        n=11;
-        x=0.91;
+        n=20;
+        x=0.35;
 var
         t: text;
+        answer: string;
 begin
         assign(t,'src/calcRes.txt');
         rewrite(t);
         writeln(t, 'n = ', n, ';');
         writeln(t, 'x = ', x, ';');
         writeln(t, 'Sum = ', calcSum(n, x),';');
-        writeln(t, 'Steps:');
-	writeln(t, 'a': 3, 'sum':10);
         close(t);
-        steps(n, x, 'src/calcRes.txt');
+        writeln('Вам нужно показать промежуточные вычисления? (yes)');
+        readln(answer);
+        if answer = 'yes' then
+        begin
+                assign(t,'src/steps.txt');
+                rewrite(t);
+	        writeln(t, 'a': 3, 'sum':10);
+                close(t);
+                steps(n, x, 'src/steps.txt');
+        end;
         write('Подсчет выполнен. Пожалуйста, проверьте файл calcRes.txt');
         readln;
 end.
