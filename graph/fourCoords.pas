21d9ccb417e35b7bc3e6c7a222d7bb724a306290
Andrey Shpak <anshpak.by@gmail.com>
2022-05-06 10:17:17 +0300

    feat: add procedure drawing coordinate system

--- graph/fourCoords.pas
+++ graph/fourCoords.pas
@@ -1,31 +1,32 @@
 program gr;
 uses graph;
 
-procedure coordSystem(xl, yh, xr, yd: word);
+procedure coordSystem(xMin, yMin, xMax, yMax: word);
 var
         x0, y0, i: word;
 begin
-        x0 := (xl + xr) div 2;
-        y0 := (yh + yd) div 2;
-        line(xl, y0, xr, y0);
-        line(x0, yh, x0, yd);
-        line(xl, yh, x0 - 8, 10);
-        line(x0, yh, x0 + 8, 10);
-        line(xr, y0, xr - 8, y0 - 10);
-        line(xr, y0, xr - 8, y0 + 10);
-        moveto(x0 + 14, yh);
+        x0 := xMax div 2;
+        y0 := yMax div 2;
+        line(xMin, y0, xMax, y0);
+        line(x0, yMin, x0, yMax);
+        line(x0, yMin, x0 - 8, yMin + 10);
+        line(x0, yMin, x0 + 8, yMin + 10);
+        line(xMax, y0, xMax - 8, y0 - 10);
+        line(xMax, y0, xMax - 8, y0 + 10);
+        moveto(x0 + 14, yMin);
         outtext('y');
-        moveto(xr - 14, y0 + 14);
+        moveto(xMax - 14, y0 + 14);
         outtext('x');
         moveto(x0 - 14, y0 + 14);
         outtext('0');
         i := 1;
-        while(i * 20 <= yd) do begin
-                line(x0 - 4, i * 20, x0 + 4, i * 20);
+        while(y0 + i * 20 < yMax) do begin
+                line(x0 - 4, y0 - i * 20, x0 + 4, y0 - i * 20);
+                line(x0 - 4, y0 + i * 20, x0 + 4, y0 + i * 20);
                 inc(i);
         end;
         i := 1;
-        while(i * 20 <= xr) do begin
+        while(x0 + i * 20 < xMax ) do begin
                 line(x0 + 20 * i, y0 - 4, x0 + 20 * i, y0 + 4);
                 line(x0 - 20 * i, y0 - 4, x0 - 20 * i, y0 + 4);
                 inc(i);
@@ -38,11 +39,6 @@ var
 begin
         gd := 0;
         initgraph(gd, gm, '');
-        {errorcode := graphresult;
-        if errorcode <> grok then begin
-                writeln(grapherrormsg(errorcode));
-                halt(1);
-        end;}
         x0 := getmaxx div 2;
         y0 := getmaxy div 2;
         setcolor(Red);
@@ -50,6 +46,6 @@ begin
         line(0, y0, getmaxx, y0);
         line(x0, 0, x0, getmaxy);
         setcolor(White);
-        coordSystem(x0 div 2, y0 div 2, x0, y0);
+        coordSystem(0, 0, x0, y0);
         readln;
 end.
