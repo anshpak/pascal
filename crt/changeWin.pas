fdd74952fa2203307d980d9457f310b1a3cc8ed6
Andrey Shpak <anshpak.by@gmail.com>
2022-05-10 12:30:51 +0300

    refactor: delete extra code

--- crt/changeWin.pas
+++ crt/changeWin.pas
@@ -77,34 +77,6 @@ begin
                                 textBackground(LightCyan);
                                 clrscr;
                         end;
-
-                        {if (ch >= #72) and (ch <= #80) then
-                                c := '*'
-                        else
-                                c := ' ';
-                        case ch of
-                                #72, #141: y := y - 1;
-                                #80, #145: y := y + 1;
-                                #77, #116: x := x + 1;
-                                #75, #115: x := x - 1;
-                                #49: begin
-                                        x := 10;
-                                        y := 10;
-                                        gotoxy(x, y);
-                                end;
-                                #50: begin
-                                        x := 15;
-                                        y := 15;
-                                        gotoxy(x, y);
-                                end;
-                                #51: begin
-                                        x := 20;
-                                        y := 20;
-                                        gotoxy(x, y);
-                                end;
-                        end;
-                        gotoxy(x, y);
-                        write(c);}
                 end;
         until ch = #27;
 
