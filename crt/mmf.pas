eefb92b251c83ab683792cce78caa6eb53966217
Andrey Shpak <anshpak.by@gmail.com>
2022-05-10 12:24:15 +0300

    init: start mmf task

---
+++ crt/mmf.pas
@@ -0,0 +1,11 @@
+program mmf;
+uses crt;
+const
+        M: array[1..10][1..10] of char = (('0','0','0',
+                                         ));
+        F:
+begin
+        textMode(3 + 256);
+        textBackgroundColor(Black);
+        clrscr;
+end.
