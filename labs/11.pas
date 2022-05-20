57f094e62d0ae7d0f29eabbbf570cf3cb326b6d1
Andrey Shpak <anshpak.by@gmail.com>
2022-05-20 11:03:07 +0300

    feat: update files

--- labs/11.pas
+++ labs/11.pas
@@ -1,28 +1,47 @@
 program temp;
+{$R-}
 uses crt;
 type
         letter = record
                 counter: integer;
                 symbol: char;
         end;
+        letters = array['a'..'z'] of letter;
+
+procedure countLetters(var mas: letters; st: string);
+var
+        alphabet: set of 'a'..'z';
+        ch: char;
+        i: integer;
+begin
+        alphabet := [];
+        for ch := 'a' to 'z' do
+                alphabet := alphabet + [ch];
+        for i := 1 to length(st) do begin
+                ch := lowercase(st[i]);
+                if (ch in alphabet) then
+                        inc(mas[ch].counter);
+        end;
+end;
 
 procedure getLetters(var t1, t2: text);
 var
-        poemAlph: array['a'..'z'] of letter;
+        poem: letters;
         ch: char;
         strTemp: string;
 begin
         for ch := 'a' to 'z' do begin
-                poemAlph[ch].counter := 0;
-                poemAlph[ch].symbol := ch;
+                poem[ch].counter := 0;
+                poem[ch].symbol := ch;
         end;
         read(t1, strTemp);
         writeln(strTemp);
-
+        countLetters(poem, strTemp);
         readln;
+
         for ch := 'a' to 'z' do begin
-                write(poemAlph[ch].counter, ' ');
-                write(poemAlph[ch].symbol, ' ');
+                write(poem[ch].counter, ' ');
+                write(poem[ch].symbol, ' ');
         end;
 end;
 
